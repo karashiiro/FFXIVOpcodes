6d99a226add3aa185539e76df6cc1c078c25c8d9
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-10-25 12:24:22 -0700

    stuff

--- FFXIVDataStructures/Program.cs
+++ FFXIVDataStructures/Program.cs
@@ -5,17 +5,18 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace FFXIVDataStructures
 {
-    class Program
+    public static class Program
     {
-        static async void Main(string[] _)
+        public static async Task Main(string[] _)
         {
             Console.WriteLine("Exporting...");
 
-            HttpClient http = new HttpClient();
-            Dictionary<string, string> urls = new Dictionary<string, string>
+            var http = new HttpClient();
+            var urls = new Dictionary<string, string>
             {
                 { "ServerZoneDef", "https://raw.githubusercontent.com/SapphireServer/Sapphire/develop/src/common/Network/PacketDef/Zone/ServerZoneDef.h" },
                 { "ClientZoneDef", "https://raw.githubusercontent.com/SapphireServer/Sapphire/develop/src/common/Network/PacketDef/Zone/ClientZoneDef.h" },
@@ -24,8 +25,8 @@ namespace FFXIVDataStructures
                 { "ServerChatDef", "https://raw.githubusercontent.com/SapphireServer/Sapphire/develop/src/common/Network/PacketDef/Chat/ServerChatDef.h" },
                 { "ClientChatDef", "https://raw.githubusercontent.com/SapphireServer/Sapphire/develop/src/common/Network/PacketDef/Chat/ClientChatDef.h" },
             };
-            Regex splitLines = new Regex(@"\r\n", RegexOptions.Compiled);
-            Regex splitSpaces = new Regex(@" ", RegexOptions.Compiled);
+            var splitLines = new Regex(@"\r\n", RegexOptions.Compiled);
+            var splitSpaces = new Regex(@" ", RegexOptions.Compiled);
             var output = new Dictionary<string, IDictionary<string, IList<FieldEntry>>>
             {
                 { "ServerZoneDef", new Dictionary<string, IList<FieldEntry>>() },
@@ -41,16 +42,16 @@ namespace FFXIVDataStructures
                 var res = await http.GetAsync(url.Value);
                 if (!res.IsSuccessStatusCode) // A couple of these don't exist yet
                     return;
-                string data = await res.Content.ReadAsStringAsync();
+                var data = await res.Content.ReadAsStringAsync();
 
                 IList<string> lines = splitLines.Split(data);
 
                 var structName = string.Empty;
                 var curOffset = 0;
                 var fields = new List<FieldEntry>();
-                foreach (string iline in lines)
+                foreach (var iline in lines)
                 {
-                    string line = iline.Trim();
+                    var line = iline.Trim();
 
                     if (line.IndexOf("<") != -1)
                     {
@@ -60,28 +61,28 @@ namespace FFXIVDataStructures
                     if (line.StartsWith("/") || line == string.Empty)
                         continue;
 
-                    string[] words = splitSpaces.Split(line);
+                    var words = splitSpaces.Split(line);
                     var cppTypeName = words[0];
                     if (cppTypeName == "unsigned")
                     {
-                        string fixedTypeName = string.Empty;
-                        if (words[1] == "char")
-                            fixedTypeName = "uint8_t";
-                        else if (words[1] == "short")
-                            fixedTypeName = "uint16_t";
-                        else if (words[1] == "int")
-                            fixedTypeName = "uint32_t";
+                        var fixedTypeName = words[1] switch
+                        {
+                            "char" => "uint8_t",
+                            "short" => "uint16_t",
+                            "int" => "uint32_t",
+                            _ => string.Empty
+                        };
                         words = words[2..].Prepend(fixedTypeName).ToArray();
                         cppTypeName = fixedTypeName;
                     }
 
-                    bool isArray = false;
-                    int arraySize = -1;
+                    var isArray = false;
+                    var arraySize = -1;
                     if (cppTypeName.IndexOf("[") != -1)
                     {
                         isArray = true;
-                        string arraySizeStr = cppTypeName.Substring(cppTypeName.IndexOf("[") + 1, cppTypeName.IndexOf("]"));
-                        if (!int.TryParse(arraySizeStr, out int readSize))
+                        var arraySizeStr = cppTypeName.Substring(cppTypeName.IndexOf("[") + 1, cppTypeName.IndexOf("]"));
+                        if (!int.TryParse(arraySizeStr, out var readSize))
                         {
                             arraySize = (int)Convert.ToUInt32(arraySizeStr);
                         }
@@ -92,35 +93,41 @@ namespace FFXIVDataStructures
                         cppTypeName = cppTypeName.Substring(0, cppTypeName.IndexOf("["));
                     }
 
-                    int cppTypeSize = 0;
-                    bool isSigned = false;
-                    if (cppTypeName == "uint8_t" || cppTypeName == "char")
-                        cppTypeSize = 1;
-                    else if (cppTypeName == "uint16_t")
-                        cppTypeSize = 2;
-                    else if (cppTypeName == "uint32_t")
-                        cppTypeSize = 4;
-                    else if (cppTypeName == "uint64_t")
-                        cppTypeSize = 8;
-                    else if (cppTypeName == "int8_t")
+                    var cppTypeSize = 0;
+                    var isSigned = false;
+                    switch (cppTypeName)
                     {
-                        cppTypeSize = 1;
-                        isSigned = true;
-                    }
-                    else if (cppTypeName == "int16_t")
-                    {
-                        cppTypeSize = 2;
-                        isSigned = true;
-                    }
-                    else if (cppTypeName == "int32_t" || cppTypeName == "float")
-                    {
-                        cppTypeSize = 4;
-                        isSigned = true;
-                    }
-                    else if (cppTypeName == "int64_t" || cppTypeName == "double")
-                    {
-                        cppTypeSize = 8;
-                        isSigned = true;
+                        case "uint8_t":
+                        case "char":
+                            cppTypeSize = 1;
+                            break;
+                        case "uint16_t":
+                            cppTypeSize = 2;
+                            break;
+                        case "uint32_t":
+                            cppTypeSize = 4;
+                            break;
+                        case "uint64_t":
+                            cppTypeSize = 8;
+                            break;
+                        case "int8_t":
+                            cppTypeSize = 1;
+                            isSigned = true;
+                            break;
+                        case "int16_t":
+                            cppTypeSize = 2;
+                            isSigned = true;
+                            break;
+                        case "int32_t":
+                        case "float":
+                            cppTypeSize = 4;
+                            isSigned = true;
+                            break;
+                        case "int64_t":
+                        case "double":
+                            cppTypeSize = 8;
+                            isSigned = true;
+                            break;
                     }
 
                     var fieldEntry = new FieldEntry
@@ -136,21 +143,19 @@ namespace FFXIVDataStructures
                     curOffset += cppTypeSize;
                     fields.Add(fieldEntry);
 
-                    if (line.IndexOf("}") != -1)
-                    {
-                        output[url.Key].Add(structName, fields);
+                    if (line.IndexOf("}") == -1) continue;
+                    output[url.Key].Add(structName, fields);
 
-                        structName = string.Empty;
-                        fields = new List<FieldEntry>();
-                    }
+                    structName = string.Empty;
+                    fields = new List<FieldEntry>();
                 }
             }
 
             var path1 = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", "structures.json");
-            File.WriteAllText(path1, JsonConvert.SerializeObject(output, Formatting.Indented));
+            await File.WriteAllTextAsync(path1, JsonConvert.SerializeObject(output, Formatting.Indented));
 
             var path2 = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", "structures.min.json");
-            File.WriteAllText(path2, JsonConvert.SerializeObject(output));
+            await File.WriteAllTextAsync(path2, JsonConvert.SerializeObject(output));
 
             Console.WriteLine("Done!");
             Console.ReadLine();
