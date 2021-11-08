2cdacb7b2172b6ecfa80587f45f0ea3b5c749b3b
Ra-Workspace <ra.workspace@gmail.com>
2021-11-08 15:22:20 +0900

    KR 5.551 update

--- FFXIVConstants/KR.cs
+++ FFXIVConstants/KR.cs
@@ -6,7 +6,7 @@ namespace FFXIVConstants
     {
         public static Dictionary<string, object> Constants = new Dictionary<string, object>
         {
-            { "InventoryOperationBaseValue", 0x0273 },
+            { "InventoryOperationBaseValue", 0x01E8 },
         };
     }
 }
