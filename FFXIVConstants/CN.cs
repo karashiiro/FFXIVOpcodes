8b43f80cc189d662a7b8a3319986b7b382935759
zhyupe <zhyupe@users.noreply.github.com>
2022-03-17 13:06:15 +0000

    feat(ipc): update for cn patch-6.0

--- FFXIVConstants/CN.cs
+++ FFXIVConstants/CN.cs
@@ -5,7 +5,7 @@ namespace FFXIVConstants
 {
     public static class CN
     {
-        public const string Version = "6";
+        public const string Version = "6.0";
 
         public static Dictionary<string, object> Constants = new Dictionary<string, object>
         {
