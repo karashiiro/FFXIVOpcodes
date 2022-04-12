1de734a1d4687c6980f83e56577cca17d7aeb88e
Flavien Normand <contact@flavien-normand.fr>
2022-04-12 13:10:51 +0200

    6.1 Opcodes (#83)

--- FFXIVConstants/Global.cs
+++ FFXIVConstants/Global.cs
@@ -6,7 +6,7 @@ namespace FFXIVConstants
     {
         public static Dictionary<string, object> Constants = new Dictionary<string, object>
         {
-            { "InventoryOperationBaseValue", 0x03EA },
+            { "InventoryOperationBaseValue", 0x0311 },
         };
     }
 }
