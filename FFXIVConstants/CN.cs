7bd52315d71da8393a4c527c3340c2e7548620cf
zhyupe <zhyupe@users.noreply.github.com>
2022-10-11 16:40:16 +0000

    feat(ipc): update for cn patch-6.15

--- FFXIVConstants/CN.cs
+++ FFXIVConstants/CN.cs
@@ -5,11 +5,11 @@ namespace FFXIVConstants
 {
     public static class CN
     {
-        public const string Version = "6.11";
+        public const string Version = "6.15";
 
         public static Dictionary<string, object> Constants = new Dictionary<string, object>
         {
-            { "InventoryOperationBaseValue", 0x031a },
+            { "InventoryOperationBaseValue", 0x0140 },
         };
     }
 }
