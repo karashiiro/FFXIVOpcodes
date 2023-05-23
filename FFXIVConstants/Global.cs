cda03399ee53298c50408722feadcfb7093c31c4
Flavien Normand <contact@flavien-normand.fr>
2023-05-23 14:23:09 +0200

    Patch 6.4

--- FFXIVConstants/Global.cs
+++ FFXIVConstants/Global.cs
@@ -6,8 +6,7 @@ namespace FFXIVConstants
     {
         public static Dictionary<string, object> Constants = new Dictionary<string, object>
         {
-            { "InventoryOperationBaseValue", 0x0254 },
-            { "RECV", "49 8B 40 10 4C 8B 50 38" }
+            { "InventoryOperationBaseValue", 0x02E2 }
         };
     }
 }
