2bc741cb469fe77c2ec64c6f5ba7b825d8dc19a1
Hildakoaren <2356816438@qq.com>
2025-02-23 01:15:01 +0800

    feat: update to CN 7.1

--- FFXIVConstants/CN.cs
+++ FFXIVConstants/CN.cs
@@ -5,11 +5,11 @@ namespace FFXIVConstants
 {
     public static class CN
     {
-        public const string Version = "7.05";
+        public const string Version = "7.1";
 
         public static Dictionary<string, object> Constants = new Dictionary<string, object>
         {
-            { "InventoryOperationBaseValue", 0x01b6 },
+            { "InventoryOperationBaseValue", 0x01C1 },
         };
     }
 }
