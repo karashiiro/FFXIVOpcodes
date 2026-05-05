2898b82893200e64cc8ae1d02e1eddb80a4f33e8
Ra-Workspace <ra.workspace@gmail.com>
2026-05-05 18:07:49 +0900

    Global/CN 7.5 update

--- FFXIVOpcodes/Program.cs
+++ FFXIVOpcodes/Program.cs
@@ -17,7 +17,7 @@ namespace FFXIVOpcodes
             Console.WriteLine("Exporting...");
 
             var regions = new List<RegionSet>{
-                new RegionSet { Region = "Global", Version = "6.05"},
+                new RegionSet { Region = "Global", Version = "7.5"},
                 new RegionSet { Region = "CN", Version = FFXIVConstants.CN.Version }, // <3
                 new RegionSet { Region = "KR", Version = "7.5" },
                 new RegionSet { Region = "TW", Version = "7.1" },
